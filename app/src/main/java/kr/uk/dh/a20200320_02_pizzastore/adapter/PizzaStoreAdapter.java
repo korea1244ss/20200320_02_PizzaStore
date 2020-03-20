9fb207b27868241cfa06fabab4a218ca82bd2438
korea1244ss <61854931+korea1244ss@users.noreply.github.com>
2020-03-20 12:50:38 +0900

    인터넷 권한 획득 => 웹상의 로고 출력   - 파파존스 로그는 아직 안나옴. (Oreo 이후의 폰)

--- app/src/main/java/kr/uk/dh/a20200320_02_pizzastore/adapter/PizzaStoreAdapter.java
+++ app/src/main/java/kr/uk/dh/a20200320_02_pizzastore/adapter/PizzaStoreAdapter.java
@@ -6,9 +6,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 
@@ -38,6 +42,19 @@ public class PizzaStoreAdapter extends ArrayAdapter<PizzaStore> {
         if (row == null) {
             row = inf.inflate(R.layout.pizza_store_list_item,null);
         }
+
+        PizzaStore data = mList.get(position);
+
+        ImageView logoImg = row.findViewById(R.id.logoImg);
+        TextView nameTxt = row.findViewById(R.id.nameText);
+
+        nameTxt.setText(data.getStoreName());
+//        다양성 상속  . 그래서 ImageView
+        Glide.with(mContext).load(data.getLogoUrl()).into(logoImg);
+
+
+
+
 
         return row;
     }
